9e124d200082ab3610477548ac3340014d32a29d
Mark Lester Acak <acakmarklester33@gmail.com>
2023-08-27 22:20:07 +0800

    Added a tailwind elements and a login page

--- components/Auth/LoginPage.vue
+++ components/Auth/LoginPage.vue
@@ -1,59 +1,82 @@
-<script setup>
+<script lang="ts" setup>
 import { ref } from 'vue';
 
 const form = ref({
   emailAddress: '',
   password: '',
 });
+
+const handleLogin = async () => {
+  await useFetch('http://localhost:8000/sanctum/csrf-cookie', {
+    credentials: 'include',
+  });
+
+  const token = useCookie('XSRF-TOKEN');
+
+  await useFetch('http://localhost:8000/login', {
+    credentials: 'include',
+    method: 'POST',
+    body: form.value,
+    headers: {
+      'X-XSRF-TOKEN': token.value as string,
+    },
+  });
+};
 </script>
 
 <template>
   <!-- component -->
   <div class="min-h-screen flex justify-center items-center bg-white">
     <div
-      class="p-10 border-[1px] w-96 border-slate-200 rounded-md flex flex-col items-center space-y-3"
+      class="p-10 border-[1px] sm:w-1/2 xl:w-[28rem] border-slate-200 rounded-md flex flex-col items-center space-y-3"
     >
-      <div class="py-8">
+      <div class="py-4">
         <NuxtLink to="/">
-          <img
-            class="-mt-10 w-28 xl:w-36"
-            src="@/assets/img/logo/famsi_logo.png"
-          />
+          <img class="w-28 xl:w-36" src="@/assets/img/logo/famsi_logo.png" />
         </NuxtLink>
       </div>
 
-      <BaseInputField
-        id="emailAddress"
-        v-model="form.emailAddress"
-        type="text"
-        label="Email Address"
-        placeholder="Email Address"
-      />
-
-      <BaseInputField
-        id="password"
-        v-model="form.password"
-        type="text"
-        label="Password"
-        placeholder="Password"
-      />
-      <div class="flex flex-col space-y-5 w-full">
-        <BaseButton
-          class="px-8 xl:px-10 py-3 mt-2 bg-gradient-to-r from-[#468ef9] to-[#0c66ee] text-white"
-        >
-          Log In
-        </BaseButton>
-        <div
-          class="flex items-center justify-center border-t-[1px] border-t-slate-300 w-full relative"
-        >
-          <div class="-mt-1 font-bod bg-white px-5 absolute">Or</div>
-        </div>
-        <BaseButton
-          class="px-8 xl:px-10 py-3 mt-2 bg-inherit text-gradient border border-[#0c66ee]"
-        >
-          Sign Up
-        </BaseButton>
+      <div class="pb-4">
+        <h1 class="text-xl font-normal">Login your account.</h1>
       </div>
+
+      <form class="w-full" @submit.prevent="handleLogin">
+        <BaseInputField
+          id="emailAddress"
+          v-model="form.emailAddress"
+          type="text"
+          label="Email Address"
+          placeholder="Email Address"
+        />
+
+        <BaseInputField
+          id="password"
+          v-model="form.password"
+          type="password"
+          label="Password"
+          placeholder="Password"
+        />
+
+        <div class="flex flex-col space-y-5 w-full">
+          <BaseButton
+            type="submit"
+            class="px-8 xl:px-10 py-3 mt-2 bg-gradient-to-r from-[#468ef9] to-[#0c66ee] text-white"
+          >
+            Log In
+          </BaseButton>
+          <div
+            class="flex items-center justify-center border-t-[1px] border-t-slate-300 w-full relative"
+          >
+            <div class="-mt-1 font-bod bg-white px-5 absolute">Or</div>
+          </div>
+          <BaseButton
+            type="button"
+            class="px-8 xl:px-10 py-3 mt-2 bg-inherit text-gradient border border-[#0c66ee]"
+          >
+            Sign Up
+          </BaseButton>
+        </div>
+      </form>
     </div>
   </div>
 </template>
--- components/base/Button.vue
+++ components/base/Button.vue
@@ -1,15 +1,14 @@
+<script setup>
+const props = defineProps({
+  type: String,
+});
+</script>
+
 <template>
   <button
+    :type="props.type"
     class="text-sm text-center rounded-full hover:shadow-md hover:shadow-[#0c66ee]/50 transition duration-300"
-    v-bind="$attrs"
-    v-on="$listeners"
   >
     <slot></slot>
   </button>
 </template>
-<script>
-export default {
-  name: 'BaseButton',
-  inheritAttrs: false,
-};
-</script>
